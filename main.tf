9263705b741037bdc82ff80d4abcb020adbf0af6
gregkl <gkluczynski@example.com>
2023-06-20 09:05:29 +0200

    fmt01

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@ locals {
 }
 
 resource "azurerm_storage_account" "securestorage" {
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  name                     = var.storage_account_name
-  account_tier             = "Standard"
-  account_replication_type = var.environment == "Production" ? "GRS" : "LRS"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  name                          = var.storage_account_name
+  account_tier                  = "Standard"
+  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
   public_network_access_enabled = false
 
   tags = local.tags
